942f0d9213f2b50885df14bd2580a2476e36a1cd
Johny Mejia <jhonnymejia100@gmail.com>
2025-05-16 08:40:37 -0600

    feat(unions): Create UnionsModule and UnionsService test suite

---
+++ src/unions/unions.module.ts
@@ -0,0 +1,13 @@
+// src/unions/unions.module.ts
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UnionsController } from './unions.controller';
+import { UnionsService } from './unions.service';
+import { Union } from './entities/union.entity';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([Union])],
+  controllers: [UnionsController],
+  providers: [UnionsService],
+})
+export class UnionsModule {}
