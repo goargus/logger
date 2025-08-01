c4d14d1cb0fb0df67b8b58e83255c14d266dcb88
Hector Rosales <hndoss@gmail.com>
2025-08-01 00:42:29 -0600

    feat(backend): register the backend api in auth0 (#33)
    * fix(backend): clean dist
    
    * fix(backend): complete docker-compose

--- backend/dist/churches/dto/create-church.dto.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.CreateChurchDto = void 0;
-class CreateChurchDto {
-    name;
-    associationId;
-}
-exports.CreateChurchDto = CreateChurchDto;
-//# sourceMappingURL=create-church.dto.js.map
