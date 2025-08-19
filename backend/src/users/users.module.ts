e858cfbb2576c5f73059f60a81a058a4b410c54d
Johny Mejia <jhonnymejia100@gmail.com>
2025-08-19 09:48:41 -0600

    feat(users): set up UsersModule with TypeORM integration and service/controller exports

---
+++ backend/src/users/users.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersService } from './users.service';
+import { UsersController } from './users.controller';
+import { User } from './user.entity';
+import { Role } from '../roles/role.entity';
+import { Entity as OrgEntity } from '../entities/entity.entity';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([User, Role, OrgEntity])],
+  providers: [UsersService],
+  controllers: [UsersController],
+  exports: [UsersService],
+})
+export class UsersModule {}
