b63666ed6ea7ebf5db22ff5b8ac158e6f4714dfd
Johny Mejia <jhonnymejia100@gmail.com>
2025-08-19 14:21:42 -0600

    refactor(users): replace direct service imports with forwardRef to RolesModule and EntitiesModule

--- backend/src/users/users.module.ts
+++ backend/src/users/users.module.ts
@@ -1,17 +1,21 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { UsersService } from './users.service';
-import { UsersController } from './users.controller';
 import { User } from './user.entity';
 import { Role } from '../roles/role.entity';
 import { Entity as OrgEntity } from '../entities/entity.entity';
-import { EntitiesService } from '../entities/entities.service';
-import { RolesService } from '../roles/roles.service';
+import { UsersService } from './users.service';
+import { UsersController } from './users.controller';
+import { RolesModule } from '../roles/roles.module';
+import { EntitiesModule } from '../entities/entities.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User, Role, OrgEntity]), EntitiesService, RolesService],
-  providers: [UsersService],
+  imports: [
+    TypeOrmModule.forFeature([User, Role, OrgEntity]),
+    forwardRef(() => RolesModule), 
+    forwardRef(() => EntitiesModule),
+  ],
   controllers: [UsersController],
+  providers: [UsersService],
   exports: [UsersService],
 })
 export class UsersModule {}
