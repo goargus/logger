6148e3b1ed5fa7051f9039559a804bbe9e34893b
Johny Mejia <jhonnymejia100@gmail.com>
2025-08-19 13:16:50 -0600

    chore(users): import EntitiesService and RolesService into UsersModule

--- backend/src/users/users.module.ts
+++ backend/src/users/users.module.ts
@@ -5,9 +5,11 @@ import { UsersController } from './users.controller';
 import { User } from './user.entity';
 import { Role } from '../roles/role.entity';
 import { Entity as OrgEntity } from '../entities/entity.entity';
+import { EntitiesService } from '../entities/entities.service';
+import { RolesService } from '../roles/roles.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User, Role, OrgEntity])],
+  imports: [TypeOrmModule.forFeature([User, Role, OrgEntity]), EntitiesService, RolesService],
   providers: [UsersService],
   controllers: [UsersController],
   exports: [UsersService],
