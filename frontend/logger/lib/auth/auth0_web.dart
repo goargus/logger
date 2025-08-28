992170cdf8b82fcebb5dde22d0867a690a045ad8
Johny Mejia <jhonnymejia100@gmail.com>
2025-08-28 10:37:19 -0600

    refactor(auth): improve code readability and comments in Auth0Web class

--- frontend/logger/lib/auth/auth0_web.dart
+++ frontend/logger/lib/auth/auth0_web.dart
@@ -1,16 +1,19 @@
 import 'dart:html' as html;
 import 'dart:js_util' as js_util;
 
+/// Bridge para invocar funciones definidas en web/auth0_bridge.js
 class Auth0Web {
+  /// Abre Universal Login
   static void login() {
     js_util.callMethod(html.window, 'auth0Login', const []);
   }
 
-
+  /// Pide un access token silenciosamente (si hay sesión),
+  /// lo guarda en localStorage y lo devuelve.
   static Future<String?> refreshTokenSilently() async {
     try {
-      final promise = js_util.callMethod(html.window, 'auth0Refresh', const []);
-      final token = await js_util.promiseToFuture<String>(promise);
+      final p = js_util.callMethod(html.window, 'auth0Refresh', const []);
+      final token = await js_util.promiseToFuture<String>(p);
       return token;
     } catch (_) {
       return null;
