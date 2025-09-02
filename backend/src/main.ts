d7fce27a96f5f31b6aa4f50f14d78bb537f7fd5c
Johny Mejia <jhonnymejia100@gmail.com>
2025-09-02 16:35:34 -0600

    feat(cors): update CORS configuration to allow requests from localhost:3000

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:8080'],
+    origin: ['http://localhost:8080', 'http://localhost:3000'],
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
