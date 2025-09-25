bf75edb6c41213a36138e49de3d85cc9e1324b58
Hector Rosales <hndoss@gmail.com>
2025-09-25 17:49:54 -0600

    feat(docker): add entrypoint script for Flutter web server with environment variables

---
+++ frontend/logger/entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+flutter run -d web-server \
+  --web-hostname=0.0.0.0 \
+  --web-port=8080 \
+  --dart-define=AUTH0_DOMAIN="${AUTH0_DOMAIN}" \
+  --dart-define=AUTH0_CLIENT_ID="${AUTH0_CLIENT_ID}" \
+  --dart-define=AUTH0_REDIRECT_URI="${AUTH0_REDIRECT_URI}" \
+  --dart-define=AUTH0_AUDIENCE="${AUTH0_AUDIENCE}"
