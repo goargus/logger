325e9467c660929b56710a327ff1881bb6d8b9a8
Johny Mejia <jhonnymejia100@gmail.com>
2025-10-13 13:30:39 -0600

    feat(users): enhance UsersModule with AdminUsersController and additional entities

--- backend/src/users/users.module.ts
+++ backend/src/users/users.module.ts
@@ -3,19 +3,22 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './user.entity';
 import { Role } from '../roles/role.entity';
 import { Entity as OrgEntity } from '../entities/entity.entity';
+import { UserRoleAssignment } from '../roles/user-role-assignment.entity';
 import { UsersService } from './users.service';
-import { UsersController } from './users.controller';
+import { UsersController, AdminUsersController } from './users.controller';
+import { IdentityResolutionService } from '../auth/identity-resolution.service';
+import { IdpIdentity } from '../idp-identities/idp-identity.entity';
 import { RolesModule } from '../roles/roles.module';
 import { EntitiesModule } from '../entities/entities.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([User, Role, OrgEntity]),
+    TypeOrmModule.forFeature([User, Role, OrgEntity, UserRoleAssignment, IdpIdentity]),
     forwardRef(() => RolesModule),
     forwardRef(() => EntitiesModule),
   ],
-  controllers: [UsersController],
-  providers: [UsersService],
+  controllers: [UsersController, AdminUsersController],
+  providers: [UsersService, IdentityResolutionService],
   exports: [UsersService],
 })
 export class UsersModule {}
