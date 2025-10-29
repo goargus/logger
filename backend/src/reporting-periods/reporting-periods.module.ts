34ec0aee2e319670e0f47f53c8bb518971aad052
Johny Mejia <jhonnymejia100@gmail.com>
2025-10-29 12:58:14 -0600

    feat(reporting-periods): enhance findAll method with query parameters and add close functionality

--- backend/src/reporting-periods/reporting-periods.module.ts
+++ backend/src/reporting-periods/reporting-periods.module.ts
@@ -1,14 +1,22 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ScheduleModule } from '@nestjs/schedule';
 import { ReportingPeriodsService } from './reporting-periods.service';
 import { ReportingPeriodsController } from './reporting-periods.controller';
+import { ReportingPeriodsSchedulerService } from './reporting-periods-scheduler.service';
 import { ReportingPeriod } from './reporting-period.entity';
 import { AuthModule } from '../auth/auth.modules';
+import { Term } from '../terms/term.entity';
+import { Entity } from '../entities/entity.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([ReportingPeriod]), AuthModule],
+  imports: [
+    TypeOrmModule.forFeature([ReportingPeriod, Term, Entity]),
+    ScheduleModule.forRoot(),
+    AuthModule,
+  ],
   controllers: [ReportingPeriodsController],
-  providers: [ReportingPeriodsService],
+  providers: [ReportingPeriodsService, ReportingPeriodsSchedulerService],
   exports: [ReportingPeriodsService],
 })
 export class ReportingPeriodsModule {}
