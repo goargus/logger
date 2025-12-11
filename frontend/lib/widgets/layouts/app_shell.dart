efd313d3a12f5e8d3044df4f204ada1011464601
Johny Mejia <jhonnymejia100@gmail.com>
2025-12-11 11:53:50 -0600

    feat: refactor AppShell to use SidebarNav and remove unused routing logic

--- frontend/lib/widgets/layouts/app_shell.dart
+++ frontend/lib/widgets/layouts/app_shell.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import '../nav/side_nav.dart';
+import '../nav/sidebar_nav.dart';
 import '../../providers/auth.dart';
 
 const double kOverlayTopInset = 64.0;
@@ -9,54 +9,21 @@ class AppShell extends ConsumerWidget {
   final Widget body;
   final String activeRoute;
 
-  static const String homeRoute = '/';
-  static const String addActivityRoute = '/activities/new';
-  static const String reportsRoute = '/reports';
-
   const AppShell({
     super.key,
     required this.body,
     required this.activeRoute,
   });
 
-  int _mapRouteToIndex(String route) {
-    switch (route) {
-      case homeRoute:
-        return 0;
-      case addActivityRoute:
-        return 1;
-      case reportsRoute:
-        return 2;
-      default:
-        return 0;
-    }
-  }
-
-  void _onNavSelect(BuildContext context, int index) {
-    switch (index) {
-      case 0:
-        if (ModalRoute.of(context)?.settings.name != homeRoute) {
-          Navigator.pushNamed(context, homeRoute);
-        }
-        break;
-      case 1:
-        if (ModalRoute.of(context)?.settings.name != addActivityRoute) {
-          Navigator.pushNamed(context, addActivityRoute);
-        }
-        break;
-      case 2:
-        if (ModalRoute.of(context)?.settings.name != reportsRoute) {
-          Navigator.pushNamed(context, reportsRoute);
-        }
-        break;
-      default:
-        break;
-    }
-  }
-
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final isWide = MediaQuery.of(context).size.width >= 1000;
+    final authState = ref.watch(authNotifierProvider);
+    final userName = authState.user?['name'] ?? 
+                     authState.user?['nickname'] ?? 
+                     'Usuario';
+    final userEmail = authState.user?['email'] ?? '';
+    final userPicture = authState.user?['picture'];
 
     return Scaffold(
       body: SafeArea(
@@ -69,9 +36,11 @@ class AppShell extends ConsumerWidget {
                 children: [
                   if (isWide) const SizedBox(width: 8),
                   if (isWide)
-                    SideNav(
-                      selectedIndex: _mapRouteToIndex(activeRoute),
-                      onSelect: (i) => _onNavSelect(context, i),
+                    SidebarNav(
+                      userName: userName,
+                      userEmail: userEmail,
+                      userPicture: userPicture,
+                      onCreateActivity: () => Navigator.pushNamed(context, '/activities/new'),
                     ),
                   Expanded(
                     child: Padding(
