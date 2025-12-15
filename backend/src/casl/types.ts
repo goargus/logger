258249582380602397d9282a6b597f52b0c969ff
Johny Mejia <jhonnymejia100@gmail.com>
2025-12-15 10:26:27 -0600

    feat: Implement CASL types for access control management

---
+++ backend/src/casl/types.ts
@@ -0,0 +1,38 @@
+import { InferSubjects, MongoAbility } from '@casl/ability';
+import { Activity } from '../activity/activity.entity';
+import { Entity } from '../entities/entity.entity';
+import { ReportingPeriod } from '../reporting-periods/reporting-period.entity';
+import { Term } from '../terms/term.entity';
+import { User } from '../users/user.entity';
+import { Role } from '../roles/role.entity';
+import { ActivityType } from '../activities-type/activity-type.entity';
+
+export enum Action {
+  Manage = 'manage',
+  Create = 'create',
+  Read = 'read',
+  Update = 'update',
+  Delete = 'delete',
+}
+
+export type Subjects =
+  | InferSubjects<
+      | typeof Activity
+      | typeof Entity
+      | typeof ReportingPeriod
+      | typeof Term
+      | typeof User
+      | typeof Role
+      | typeof ActivityType
+    >
+  | 'all';
+
+export type AppAbility = MongoAbility<[Action, Subjects]>;
+
+export interface IPolicyHandler {
+  handle(ability: AppAbility): boolean;
+}
+
+export type PolicyHandlerCallback = (ability: AppAbility) => boolean;
+
+export type PolicyHandler = IPolicyHandler | PolicyHandlerCallback;
