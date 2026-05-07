da68209d9f08dc71033476f925bcc48310735e9a
Johny Mejia <jhonnymejia100@gmail.com>
2026-05-07 06:55:44 -0600

    feat(admin): wire admin activity dependencies

--- backend/src/admin/admin.module.ts
+++ backend/src/admin/admin.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AdminController } from './admin.controller';
 import { Reflector } from '@nestjs/core';
+import { ActivitiesModule } from '../activity/activities.module';
+import { PeriodsModule } from '../periods/periods.module';
+import { User } from '../users/user.entity';
+import { ActivityType } from '../activities-type/activity-type.entity';
 
 @Module({
+  imports: [ActivitiesModule, PeriodsModule, TypeOrmModule.forFeature([User, ActivityType])],
   controllers: [AdminController],
   providers: [Reflector],
 })
